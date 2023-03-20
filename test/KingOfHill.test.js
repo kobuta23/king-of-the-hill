86809be42cd33ab74cfd87ae5ccc48c1aaf42084
Axe <ngsmo@iscte-iul.pt>
2023-03-20 19:59:05 +0000

    fix tests & add new tests

--- test/KingOfHill.test.js
+++ test/KingOfHill.test.js
@@ -1,7 +1,6 @@
 const { Framework } = require("@superfluid-finance/sdk-core")
 const { ethers , network} = require("hardhat")
 const { assert, expect } = require("chai")
-const KingOfHillArtifact = require("../artifacts/contracts/KingOfHill.sol/KingOfHill.json")
 const { deployTestFramework } = require("@superfluid-finance/ethereum-contracts/dev-scripts/deploy-test-framework");
 const TestToken = require("@superfluid-finance/ethereum-contracts/build/contracts/TestToken.json")
 
@@ -21,8 +20,8 @@ let users
 let op
 let employmentLoan
 
-const alotOfEth = ethers.utils.parseEther("100000")
-const someEth = ethers.utils.parseEther("10000")
+const alotOfEth = ethers.utils.parseEther("100000");
+const someEth = ethers.utils.parseEther("10000");
 
 before(async function () {
     //get accounts from hardhat
@@ -60,7 +59,7 @@ before(async function () {
         .deployPureSuperToken(
             "Army Token",
             "ARMY",
-            ethers.utils.parseUnits("1000000000000")
+            ethers.utils.parseUnits("100000000000000000000000000000")
         );
 
     daix = await sf.loadSuperToken("fDAIx");
@@ -78,24 +77,24 @@ before(async function () {
             providerOrSigner: alice
         })
     }
-    Object.assign(daix, {netFlow, logBal, bal});
-    Object.assign(army, {netFlow, logBal, bal});
 
-    daix.logBal(admin);
-    daix.netFlow(admin);
+    async function userFlow(senderName, receiverName) {
+        const result = await this.getFlow({
+            sender: senderName.address,
+            receiver: receiverName.address,
+            providerOrSigner: alice
+        });
+        return result.flowRate;
+    }
+
+    Object.assign(daix, {netFlow, userFlow, logBal, bal});
+    Object.assign(army, {netFlow, userFlow, logBal, bal});
 
     dai = new ethers.Contract(daix.underlyingToken.address, TestToken.abi, admin);
-
-    const KingOfHill = await ethers.getContractFactory("KingOfHill", admin)
-    hill = await KingOfHill.deploy(
-        daix.address,  // cashToken
-        army.address,  // armyToken
-        (1e10).toString() // decay
-    )
-    hill.name = "hill "
-    await hill.deployed()
-    await army.transfer({receiver: hill.address, amount: ethers.utils.parseUnits("1000000000000")}).exec(admin)
     users = [hill, admin, alice, bob, carol, david];
+
+    console.log(`daix address: ${daix.address}`);
+    console.log(`army address: ${army.address}`);
 })
 
 beforeEach(async function () {
@@ -107,94 +106,167 @@ beforeEach(async function () {
     await daix.transfer({receiver: bob.address, amount: someEth}).exec(admin);
     await daix.transfer({receiver: carol.address, amount: someEth}).exec(admin);
     await daix.transfer({receiver: david.address, amount: someEth}).exec(admin);
+
+    // deploy fresh app each time
+    const KingOfHill = await ethers.getContractFactory("KingOfHill", admin);
+    hill = await KingOfHill.deploy(
+        daix.address,  // cashToken
+        army.address,  // armyToken
+        (1e10).toString(), // decay
+        (1e10).toString() // step
+    )
+    hill.name = "hill";
+    await hill.deployed();
+    await army.transfer({receiver: hill.address, amount: ethers.utils.parseUnits("1000000000000")}).exec(admin);
 })
 
-describe("intial stream testing", async function () {
+afterEach(async function () {
+    await noAppJailed();
+});
+describe("test deployment", async function () {
+
+    it("should deploy new hill", async () => {
+        assert.equal(await hill.cashToken(), daix.address, "cashToken should be daix");
+        assert.equal(await hill.armyToken(), army.address, "armyToken should be army");
+        assert.equal(await hill.decay(), (1e10).toString(), "decay should be 1e10");
+        assert.equal(await hill.step(), (1e10).toString(), "step should be 1e10");
+        assert.equal(await hill.king(), admin.address, "king should be admin");
+    });
+
+    // should revert if decay is not positive
+    it("should revert if decay is non positive", async () => {
+        const HillFactory = await ethers.getContractFactory("KingOfHill", admin)
+        await expect(HillFactory.deploy(
+            daix.address,  // cashToken
+            army.address,  // armyToken
+            "-1", // decay
+            (1e10).toString()
+        )).to.be.revertedWith("decay must be positive")
+    });
+});
+
+describe("initial stream testing", async function () {
+
     it("user streams and should get a stream back", async function () {
         //alice streams cash to the hill
         op = await daix.createFlow({
             receiver: hill.address,
-            flowRate: "10000000000" //
+            flowRate: "10000000000"
         });
-        try {
-            await op.exec(alice);
-        } catch (e){
-            console.log("caught smth, ");
-        }
+        await op.exec(alice);
 
-        //cash netflowrate
-        assert.isBelow(Number(await daix.netFlow(alice)), 0, "netflow should be negative")
-        assert.isAbove(Number(await army.netFlow(alice)), 0, "netflow should be positive")
+        assert.isBelow(Number(await daix.netFlow(alice)), 0, "netflow should be negative");
+        assert.isAbove(Number(await army.netFlow(alice)), 0, "netflow should be positive");
+        assert.equal(Number(await daix.userFlow(alice, hill)), 10000000000, "alice should be streaming to hill");
+        assert.equal(Number(await army.userFlow(hill, alice)), Number(await hill.armyFlowRate("10000000000")), "hill should be streaming to alice");
+
+        assert.isAbove(Number(await daix.netFlow(admin)), 0, "hill should be streaming cash");
+
         await cleanUp([alice]);
-    })
+        await noStreamToKing();
+    });
 
     it("users who stream more, get more back", async function () {
         //alice streams cash to the hill
         op = await daix.createFlow({
             receiver: hill.address,
-            flowRate: "10000000000" //
+            flowRate: "10000000000"
         });
-        try {
-            await op.exec(alice);
-        } catch (e){
-            console.log("caught smth, ");
-        }
+        await op.exec(alice);
+        assert.equal(Number(await daix.userFlow(alice, hill)), 10000000000, "alice should be streaming to hill");
         op = await daix.createFlow({
             receiver: hill.address,
-            flowRate: "20000000000" //
+            flowRate: "20000000000"
         });
-        try {
-            await op.exec(bob);
-        } catch (e){
-            console.log("caught smth, ");
-        }
-        //cash netflowrate
-        let x = Number(await army.netFlow(alice));
-        let y = Number(await army.netFlow(bob));
-        console.log (x, " vs ", y);
+        await op.exec(bob);
+        assert.equal(Number(await daix.userFlow(bob, hill)), 20000000000, "alice should be streaming to hill");
+
+        let x = Number(await army.userFlow(hill, alice));
+        let y = Number(await army.userFlow(hill, bob));
+        // console.log(`${x} vs ${y}`);
         assert.isBelow(x, y, "Bob getting more than alice")
-        await cleanUp([alice, bob])
-    })
+        await cleanUp([alice, bob]);
+        await noStreamToKing();
+    });
 
     it("users streaming later get lower army flow", async function () {
-        //alice streams cash to the hill
         op = await daix.createFlow({
             receiver: hill.address,
-            flowRate: "10000000000" //
+            flowRate: "10000000000"
         });
-        try {
-            await op.exec(bob);
-            await pass1week();
-            await op.exec(carol);
-        } catch (e){
-            console.log("caught smth, ");
-        }
 
-        //cash netflowrate
-        let x = Number(await army.netFlow(bob));
-        let y = Number(await army.netFlow(carol));
-        console.log (x, " vs ", y);
-        assert.isAbove(x , y, "Bob isn't getting more than carol. even tho he started earlier");
-        await cleanUp([bob, carol])
-    })
+        await op.exec(bob);
+        await pass1week();
+        await op.exec(carol);
+        await pass1week();
+        await op.exec(alice);
+
+        let [x, y, z] = await Promise.all([army.netFlow(bob), army.netFlow(carol), army.netFlow(alice)]);
+        // console.log(`${x} vs ${y} vs ${z}`);
+        assert.isAbove(Number(x) , Number(y), "Bob isn't getting more than Carol. even tho he started earlier");
+        assert.isAbove(Number(y) , Number(z), "Carol isn't getting more than Alice. even tho he started earlier");
+        await cleanUp([alice, bob, carol]);
+        await noStreamToKing();
+    });
+
 });
+
+describe("became king", async function () {
+
+    // user transfer cashToken to hill and becomes king (ERC777 callback)
+    it("user transfer cashToken to hill and becomes king (ERC777 callback)", async function () {
+        // admin funds alice
+        await army.transfer({receiver: alice.address, amount: ethers.utils.parseUnits("10")}).exec(admin);
+        // assert that alice is not king
+        assert.notEqual(await hill.king(), alice.address, "alice is already king");
+        // alice will become king
+        const tx = await army.contract.connect(alice).send(
+            hill.address,
+            (1e18 + 50000000).toString(),
+            "0x"
+        );
+        // assert that alice is king
+        assert.equal(await hill.king(), alice.address, "alice is not king");
+    });
+
+    // user transfer cashToken to hill and becomes king by calling claim
+    it("user transfer cashToken to hill and becomes king by calling claim", async function () {
+        // alice becomes king by calling claim
+        assert.notEqual(await hill.king(), alice.address, "alice is already king");
+        await army.transfer({receiver: alice.address, amount: ethers.utils.parseUnits("10")}).exec(admin);
+        await army.approve({receiver: hill.address, amount: ethers.utils.parseUnits("10")}).exec(alice);
+        await hill.connect(alice).claim(ethers.utils.parseUnits("2"));
+        // assert that alice is king
+        assert.equal(await hill.king(), alice.address, "alice is not king");
+    });
+
+    it("user can't become king if he doesn't send enough cashToken", async function () {
+        // alice becomes king by calling claim
+        assert.notEqual(await hill.king(), alice.address, "alice is already king");
+        await army.transfer({receiver: alice.address, amount: ethers.utils.parseUnits("10")}).exec(admin);
+        await army.approve({receiver: hill.address, amount: ethers.utils.parseUnits("10")}).exec(alice);
+        await expect(hill.connect(alice).claim("10000")).to.be.revertedWith("send bigger army");
+    });
+});
+
 describe("kingmakers", async function () {
+
     it("outflow == sum of inflows - treasure", async function () {
-        //alice streams cash to the hill
         op = await daix.createFlow({
             receiver: hill.address,
-            flowRate: "10000000000" //
+            flowRate: "10000000000"
         });
-        try {
-            await op.exec(alice);
-            await op.exec(bob);
-            await op.exec(carol);
-        } catch (e){
-            console.log("caught smth, ");
-        }
-        let x = Number(await daix.netFlow(hill));
-        let y = Number(await daix.netFlow(admin));
-        assert.equal(x + y, 10000000000*3, "netflow isn't equal");
+
+
+        await op.exec(alice);
+        await op.exec(bob);
+        await op.exec(carol);
+
+        const kingAddress = await hill.king();
+        let taxRate = await hill.taxRate();
+        let [x, y] = await Promise.all([daix.netFlow(hill), daix.userFlow(hill, {address: kingAddress})]);
+        assert.equal(Number(taxRate), Number(y), "king not collecting full tax amount");
+        assert.equal(Number(x) + Number(y), 10000000000 * 3, "netflow isn't equal");
         await cleanUp([alice,bob,carol]);
     })
 
@@ -202,83 +274,52 @@ describe("kingmakers", async function () {
         //alice streams cash to the hill
         op = await daix.createFlow({
             receiver: hill.address,
-            flowRate: "10000000000" //
+            flowRate: "10000000000"
         });
-        try {
-            await op.exec(alice);
-            await op.exec(bob);
-            await op.exec(carol);
-        } catch (e){
-            console.log("caught smth, ");
-        }
+
+        await op.exec(alice);
+        await op.exec(bob);
+        await op.exec(carol);
 
         await pass1week(); 
         let x,y;
         x = Number(await daix.netFlow(hill)) * 86400 * 7;
         y = Number(await daix.bal(hill));
-        console.log(x, "  & ", y);
+        // console.log(x, "  & ", y);
         assert.isAtLeast(y, x, "treasure is less than expected");
         await cleanUp([alice,bob,carol]);
     });
 
-    
-
     it("check the treasure and taxflow move to new king", async function () {
-        //alice streams cash to the hill
+
         op = await daix.createFlow({
             receiver: hill.address,
-            flowRate: "10000000000" //
+            flowRate: "10000000000"
         });
-        try {
-            await op.exec(alice);
-            await op.exec(bob);
-            await op.exec(carol);
-        } catch (e){
-            console.log("caught smth");
-        }
+
+        await op.exec(alice);
+        await op.exec(bob);
+        await op.exec(carol);
+
         await pass1week();
-        balBefore = Number(await daix.bal(hill));
-        assert.isAbove(balBefore, 0, "treasure not accumulating");
+        assert.isAbove(Number(await daix.bal(hill)), 0, "treasure not accumulating");
+
+
+        const aliceBeforeNetFlow = Number(await daix.netFlow(alice));
         op = await army.approve({
             receiver: hill.address,
-            amount: "1000000000"
+            amount: "20000000000"
         })
-        aliceBeforeNetFlow = Number(await daix.netFlow(alice));
-        try{
-            await op.exec(alice)
-        } catch (e){
-            console.log("alice couldn't approve... ")
-        }
-        op = await hill.connect(alice).claim("1000000000");
-        aliceAfterNetFlow = Number(await daix.netFlow(alice));
-        console.log(aliceBeforeNetFlow, " vs ", aliceAfterNetFlow); 
+        await op.exec(alice)
+        await hill.connect(alice).claim("20000000000");
+        const aliceAfterNetFlow = Number(await daix.netFlow(alice));
+        // console.log(aliceBeforeNetFlow, " vs ", aliceAfterNetFlow);
         assert.isAbove(aliceAfterNetFlow, aliceBeforeNetFlow, "stream not redirected properly");
-        
         assert.equal(Number(await daix.bal(hill)), 0, "treasure didn't transfer completely");
-        
         await cleanUp([alice,bob,carol]);
-        await cleanUp([alice], army);
     });
 
-    
 });
-/*describe("fuzzing", async function () {
-    it("tries a bunch of random scenarios", async function () {
-        //alice streams cash to the hill
-        players = [alice, bob, carol, david];
-
-        for(let i=0;i<20;i++){
-            // set up random action taker
-            // 1. Open a stream of DAIx
-            // 2. Check if can be 
-
-
-            // invariant checks
-
-            // 
-        }
-    });
-});*/
 
 async function logBals(token, users){
     console.log(`${await token.symbol({providerOrSigner: alice})} BALANCES:`)
@@ -293,11 +334,23 @@ async function pass1week(){
     await network.provider.send("evm_mine")
 }
 
-async function cleanUp(users, token = daix){
-    await users.forEach(async user => {
+async function cleanUp(users, token = daix) {
+    for(const user of users) {
         await token.deleteFlow({
             sender: user.address,
             receiver: hill.address
-        }).exec(user)
-    });
+        }).exec(user);
+    };
+}
+
+async function noAppJailed(){
+    const jailed = await sf.host.contract.connect(admin).isAppJailed(hill.address)
+    assert.equal(jailed, false, "app is jailed");
+}
+
+async function noStreamToKing() {
+    const kingAddress= await hill.king();
+    const king = {address: kingAddress};
+    const stream = await daix.userFlow(hill, king);
+    assert.equal(Number(stream), 0, "stream to king");
 }
