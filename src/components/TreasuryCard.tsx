4248ba04232e5ea233d0f63029d00c88304a17ee
Mikk <mikk@superfluid.finance>
2023-03-24 15:33:52 +0200

    Flowing treasury

--- src/components/TreasuryCard.tsx
+++ src/components/TreasuryCard.tsx
@@ -1,8 +1,11 @@
-import { FC } from "react";
+import { BigNumber } from "ethers";
+import { FC, useMemo } from "react";
 import styled from "styled-components";
 import { useBalance } from "wagmi";
 import network from "../configuration/network";
+import { subgraphApi } from "../redux/store";
 import Amount from "./Amount";
+import FlowingBalance from "./FlowingBalance";
 import Paper from "./Paper";
 import { H4, H6, H7 } from "./Typography";
 
@@ -13,17 +16,40 @@ const StyledTreasuryCard = styled(Paper)`
 interface TreasuryCardProps {}
 
 const TreasuryCard: FC<TreasuryCardProps> = ({}) => {
+  const cashSnapshotResponse = subgraphApi.useAccountTokenSnapshotQuery({
+    chainId: network.id,
+    id: `${network.hillAddress}-${network.cashToken}`,
+  });
+
   const { data: cashBalance } = useBalance({
     address: network.hillAddress,
     token: network.cashToken,
   });
 
+  const snapshot = useMemo(() => {
+    if (!cashSnapshotResponse.data) return;
+    const { balanceUntilUpdatedAt, updatedAtTimestamp, totalInflowRate } =
+      cashSnapshotResponse.data;
+
+    const taxFlowRate = BigNumber.from(totalInflowRate).div(10).toString();
+
+    return {
+      balanceUntilUpdatedAt,
+      updatedAtTimestamp,
+      totalNetFlowRate: taxFlowRate,
+    };
+  }, [cashSnapshotResponse.data]);
+
   return (
     <StyledTreasuryCard>
       <H6>Hill Treasury</H6>
-      {cashBalance && (
+      {snapshot && (
         <H4>
-          <Amount wei={cashBalance.value} />
+          <FlowingBalance
+            balance={snapshot.balanceUntilUpdatedAt}
+            balanceTimestamp={snapshot.updatedAtTimestamp}
+            flowRate={snapshot.totalNetFlowRate}
+          />
         </H4>
       )}
       <H7>$CASH</H7>
